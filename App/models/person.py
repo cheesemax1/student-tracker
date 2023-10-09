709b159cf3ce226a58cceb814e82f8a2b6d2a222
uwi-jmoore <109738746+uwi-jmoore@users.noreply.github.com>
2023-10-09 20:42:32 +0000

    Formating person

--- App/models/person.py
+++ App/models/person.py
@@ -1,19 +1,20 @@
 from App.database import db
-class Person(db.Model):
-    __abstract__ = True
-    id = db.Column(db.Integer, primary_key = True)
-    first_name = db.Column(db.String, nullable = False)
-    last_name = db.Column(db.String, nullable = False)
-    
-    def __init__(self, id, first_name,last_name):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-    
-    def toJSON(self):
-        return {
-            "id": self.id, 
-            "first_name": self.first_name,
-            "last_name": self.last_name
-            }
 
+
+class Person(db.Model):
+  __abstract__ = True
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String, nullable=False)
+  last_name = db.Column(db.String, nullable=False)
+
+  def __init__(self, id, first_name, last_name):
+    self.id = id
+    self.first_name = first_name
+    self.last_name = last_name
+
+  def toJSON(self):
+    return {
+        "id": self.id,
+        "first_name": self.first_name,
+        "last_name": self.last_name
+    }

a4b5d98b4fa92f608b1a5bea734e2938b545fcf2
uwi-jmoore <109738746+uwi-jmoore@users.noreply.github.com>
2023-10-10 00:55:18 +0000

    fixing datetime issue in student model

--- App/models/student.py
+++ App/models/student.py
@@ -1,24 +1,29 @@
 from App.database import db
 from .person import *
+
+
 class Student(Person):
-    __tablename__ = 'student'
-    student_id = db.Column(db.Integer,nullable=False)
-    year = db.Column(db.Date,nullable=False,default=datetime.utcnow())
-    karma = db.Column(db.Integer,default=0)
-    studentreviews = db.relationship('review',backref=db.backref('student'),lazy='joined')
+  __tablename__ = 'student'
+  student_id = db.Column(db.Integer, nullable=False)
+  year = db.Column(db.Date, nullable=False)
+  karma = db.Column(db.Integer, default=0)
+  studentreviews = db.relationship('review',
+                                   backref=db.backref('student'),
+                                   lazy='joined')
 
-    def __repr__(self):
-        return f'<Student :{self.first_name, self.last_name}, Year :{self.year}, Karma: {self.karma}>'
 
-    def __init__(self,name,year,karma=0):
-        self.name = name
-        self.year = year
-        self.karma = karma
+def __repr__(self):
+  return f'<Student :{self.name}, Year :{self.year}, Karma: {self.karma}>'
 
-    def toJSON(self):
-        return{
-            'id': self.id,
-            'name': self.name,
-            'year': self.year,
-            'karma': self.karma
-        }
+  def __init__(self, name, year, karma=0):
+    self.name = name
+    self.year = year
+    self.karma = karma
+
+  def toJSON(self):
+    return {
+        'id': self.id,
+        'name': self.name,
+        'year': self.year,
+        'karma': self.karma
+    }

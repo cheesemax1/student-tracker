5b631716df0b32d40d2adf6e73a0e8aaee33001e
uwi-jmoore <109738746+uwi-jmoore@users.noreply.github.com>
2023-10-11 00:09:33 +0000

    fixing import issue

--- App/views/user.py
+++ App/views/user.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, json, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
-from flask_jwt_extended import jwt_required, current_identity
+from flask_jwt import current_identity
 from flask_login import current_user, login_required
 
 from .index import index_views
 
 from App.controllers import (
     create_user,
-    jwt_authenticate,
+    # jwt_authenticate,
     get_all_users,
     get_all_users_json,
-    jwt_required,
+    # jwt_required,
     is_admin,
     get_user,
 )
